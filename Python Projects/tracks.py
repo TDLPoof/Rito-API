71e2c3388157f4ac1ce1d0fde4b8c7757be72ce4
rtripathi <rtripathi@riotgames.com>
2018-08-02 10:28:05 -0500

    Tracks

---
+++ Python Projects/tracks.py
@@ -0,0 +1,9 @@
+# Create a database called tracks.db
+# Create 3 tables:
+#    1. Artist (id, name)
+#    2. Album (id, artist_id, title)
+#    3. Track (id, title, album_id, track_duration_in_seconds, rating, play_count)
+# Study the file "Library.xml". Look at how the data is structured.
+# For each track that occurs in the file, insert records into the Artist, Album and Track tables.
+# If the Artist already exists, or if the Album already exists, then you should not insert a new entry in those tables.
+# Make sure that the "id" fields are primary keys with auto-increment.
